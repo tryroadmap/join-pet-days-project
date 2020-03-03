20cf4d70f65e67939fc7b44cd4b1da5640acb304
nadia <nadi1990@gmail.com>
2020-03-03 13:04:03 -0500

    Removed hardcoded package names

--- run_requirements.sh
+++ run_requirements.sh
@@ -3,6 +3,9 @@
 reqfile=${1}
 
 while read line
-do 
-echo $line | R --no-save
+do
+Rscript -e 'install.packages("'$line'", repos = "https://cloud.r-project.org/")' 
 done < $reqfile
+
+# run the fontawesome package from github till suitable replacement found
+Rscript -e 'devtools::install_github("rstudio/fontawesome")'
