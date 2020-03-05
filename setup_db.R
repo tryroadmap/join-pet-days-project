ccec8692aec467c91aa416c60eef58378d9b9660
nadia <nadi1990@gmail.com>
2020-03-04 21:29:01 -0500

    fixed data propagation and view creation - combined to 1 script

--- setup_db.R
+++ setup_db.R
@@ -1,24 +1,20 @@
-# switching all data from AWS RDS to SQLite because my free time on AWS is over
+# Create the SQLite db and tables, inserts data from csv files, and creates Views and triggers for the shiny app
 library(RSQLite)
-library(RMySQL)
-# connect to AWS and get all my data
-con <- dbConnect(MySQL(),
-                 username = Sys.getenv("RDSpetsuser"),
-                 password = Sys.getenv("RDSpetspw"),
-                 host = Sys.getenv("RDShost"),
-                 dbname = 'PetRecords')
 
-mydb <- dbConnect(RSQLite::SQLite(), "PetRecords.sqlite")
-dbSendStatement(mydb, "DROP VIEW viewVaccinesPetsVets")
+## DB setup
+mydb <- dbConnect(SQLite(), "PetRecords.sqlite")
 
-dimPets <- dbGetQuery(con, 'SELECT * FROM dimPets')
-dimVets <- dbGetQuery(con, 'SELECT * FROM dimVets')
-dimMeds <- dbGetQuery(con, 'SELECT * FROM dimMeds')
-dimTests <- dbGetQuery(con, 'SELECT * FROM dimTests')
-dimVaccines <- dbGetQuery(con, 'SELECT * FROM dimVaccines')
-dimVisits <- dbGetQuery(con, 'SELECT * FROM dimVisits')
 
-dbSendStatement(mydb, "CREATE TABLE `dimPets` (
+# read csv
+dimPets <- read.csv("assets/data/dimPets.csv")
+dimVets <- read.csv("assets/data/dimVets.csv")
+dimVisits <- read.csv("assets/data/dimVisits.csv")
+dimTests <- read.csv("assets/data/dimTests.csv")
+dimMeds <- read.csv("assets/data/dimMeds.csv")
+dimVaccines <- read.csv("assets/data/dimVaccines.csv")
+
+# create tables
+rs <- dbSendStatement(mydb, "CREATE TABLE `dimPets` (
   `pet_id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
   `pet_name` varchar(250) NOT NULL,
   `pet_dob` date,
@@ -30,9 +26,9 @@ dbSendStatement(mydb, "CREATE TABLE `dimPets` (
   `created_date` datetime NOT NULL DEFAULT (DATETIME('NOW')),
   `updated_date` datetime
 )")
+dbClearResult(rs)
 
-
-dbSendStatement(mydb, "CREATE TABLE `dimVets` (
+rs <- dbSendStatement(mydb, "CREATE TABLE `dimVets` (
   `vet_id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
   `vet_name` varchar(250) NOT NULL,
   `vet_address` varchar(250),
@@ -46,8 +42,9 @@ dbSendStatement(mydb, "CREATE TABLE `dimVets` (
   `created_date` datetime NOT NULL DEFAULT (DATETIME('NOW')),
   `updated_date` datetime
 )")
+dbClearResult(rs)
 
-dbSendStatement(mydb, "CREATE TABLE `dimVisits` (
+rs <- dbSendStatement(mydb, "CREATE TABLE `dimVisits` (
   `visit_id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
   `visit_date` date NOT NULL,
   `visit_weight` float,
@@ -63,8 +60,9 @@ dbSendStatement(mydb, "CREATE TABLE `dimVisits` (
   `pet_id` smallint(6) NOT NULL,
   `vet_id` int(11) NOT NULL
 )")
+dbClearResult(rs)
 
-dbSendStatement(mydb, "CREATE TABLE `dimVaccines` (
+rs <- dbSendStatement(mydb, "CREATE TABLE `dimVaccines` (
   `vaccine_id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
   `vaccine_name` varchar(200) NOT NULL,
   `vaccine_description` varchar(500),
@@ -80,8 +78,9 @@ dbSendStatement(mydb, "CREATE TABLE `dimVaccines` (
   `vet_id` int(11) NOT NULL,
   `visit_id` int(11) NOT NULL
 )")
+dbClearResult(rs)
 
-dbSendStatement(mydb, "CREATE TABLE `dimTests` (
+rs <- dbSendStatement(mydb, "CREATE TABLE `dimTests` (
   `test_id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
   `test_name` varchar(200) NOT NULL,
   `test_description` varchar(500),
@@ -98,8 +97,9 @@ dbSendStatement(mydb, "CREATE TABLE `dimTests` (
   `vet_id` int(11) NOT NULL,
   `visit_id` int(11) NOT NULL
 )")
+dbClearResult(rs)
 
-dbSendStatement(mydb, "CREATE TABLE `dimMeds` (
+rs <- dbSendStatement(mydb, "CREATE TABLE `dimMeds` (
   `med_id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
   `med_name` varchar(200) NOT NULL,
   `med_description` varchar(500),
@@ -117,6 +117,9 @@ dbSendStatement(mydb, "CREATE TABLE `dimMeds` (
   `visit_id` int(11) NOT NULL
 )")
 
+dbClearResult(rs)
+
+# insert data from csv files
 dbWriteTable(mydb, "dimPets", dimPets, append = TRUE)
 dbWriteTable(mydb, "dimVets", dimVets, append = TRUE)
 dbWriteTable(mydb, "dimMeds", dimMeds, append = TRUE)
@@ -124,11 +127,9 @@ dbWriteTable(mydb, "dimTests", dimTests, append = TRUE)
 dbWriteTable(mydb, "dimVaccines", dimVaccines, append = TRUE)
 dbWriteTable(mydb, "dimVisits", dimVisits, append = TRUE)
 
-dbListTables(mydb)
-new_dimVets <- dbGetQuery(mydb, 'SELECT * FROM dimVaccines')
+#dbGetQuery(mydb, "SELECT * FROM dimVaccines")
 
-#dbSendQuery(mydb, "DROP TRIGGER create_date_vets")
-
+# create triggers
 dbSendStatement(mydb, "CREATE TRIGGER update_date_pets AFTER UPDATE 
            ON dimPets
             BEGIN
@@ -165,61 +166,7 @@ dbSendStatement(mydb, "CREATE TRIGGER update_date_visits AFTER UPDATE
             UPDATE dimVisits SET updated_date = datetime('now') WHERE visit_id = new.visit_id;
             END;")
 
-dbGetQuery(mydb, "SELECT name FROM sqlite_master WHERE type = 'trigger'") 
-# dbSendStatement(mydb, "DELETE FROM dimVets WHERE vet_id = 12")
-vaccines <- dbGetQuery(mydb, "SELECT * FROM dimVaccines")
-meds <- dbGetQuery(mydb, "SELECT * FROM dimMeds")
-
-names(dimVaccines)
-
-dbSendStatement(mydb,
-  "INSERT INTO dimVets (vet_name, vet_address, vet_city, vet_state, vet_zip, vet_phone, vet_website, vet_email) 
-  VALUES ('Canyon Pet Hospital', 'East Old Canyon Court', 'Flagstaff', 'AZ', '86001', '928-774-5197', 'http://www.canyonpet.com', 'info@canyonpet.com')"
-)
-
-dbSendStatement(mydb,
-                "INSERT INTO dimVisits (visit_date, visit_weight, visit_receipt, routine_visit_summary, visit_category, visit_doctor, pet_id, vet_id) 
-                VALUES ('2018-11-20', 64.1, 'https://s3.amazonaws.com/pet-records/vaccine-certifications/20181120_lloyd_lepto.pdf', 'Lepto', 'routine', 'Harbert', 2, 1)"
-)
-
-dbSendStatement(mydb,
-                "UPDATE dimVisits SET visit_date = '2018-10-10'
-                WHERE visit_id = 42"
-)
-
-dbSendStatement(mydb,
-                "UPDATE dimMeds SET med_end_date = '2018-05-20', med_current_flag = 'N'
-                WHERE med_id = 25"
-)
-
-dbSendStatement(mydb,
-                "INSERT INTO dimVaccines (vaccine_name, vaccine_date_given, vaccine_date_expires, vaccine_notes, vaccine_current_flag, pet_id, vet_id, visit_id) 
-                VALUES ('Lepto', '2018-11-20', '2019-11-20', 'Leptospira Canicola-Grippotyphosa-Icterohaemorrhagiae-Pomona Bacterin', 'Y', 2, 1, 43)"
-)
-
-dbSendStatement(mydb,
-                "INSERT INTO dimVaccines (vaccine_name, vaccine_date_given, vaccine_date_expires, vaccine_current_flag, pet_id, vet_id, visit_id) 
-                VALUES ('Lepto', '2018-10-10', '2019-10-10', 'Y', 1, 12, 42)"
-)
-
-dbSendStatement(mydb,
-                "UPDATE dimVaccines SET vaccine_certificate = 'https://s3.amazonaws.com/pet-records/exams/20181010_layla_exam.pdf'
-                WHERE vaccine_id = 36"
-)
-
-dbSendStatement(mydb,
-                "UPDATE dimVaccines SET vaccine_current_flag = 'N'
-                WHERE vaccine_id = 22"
-)
-
-dbSendStatement(mydb,
-                "UPDATE dimVaccines SET vaccine_current_flag = 'N'
-                WHERE vaccine_id = 23"
-)
-
-# next steps are to create views and then hook up the new db to the shiny app
-# these are the views I need
-
+# create views
 dbSendStatement(mydb, "CREATE VIEW viewVisitsPets AS
   SELECT 
     dimVisits.visit_id,
@@ -243,6 +190,58 @@ dbSendStatement(mydb, "CREATE VIEW viewVisitsPets AS
   FROM dimVisits
   INNER JOIN dimPets
   ON dimVisits.pet_id = dimPets.pet_id")
+
+dbSendStatement(mydb, "CREATE VIEW viewRoutineMedHistTimeline AS
+  SELECT 'routine' AS 'group', 
+    visit_id || '_' || 'routine' AS id,
+    pet_name,
+    vet_name,
+    visit_date AS 'start',
+    routine_visit_summary AS content,
+    visit_category AS category
+  FROM viewVisitsPetsVets
+  WHERE visit_category like '%routine' AND routine_visit_summary not like 'Initial visit'
+  UNION
+  SELECT 'med' AS 'group', 
+    visit_id || '_' || 'med' AS id,
+    pet_name,
+    vet_name,
+    visit_date AS 'start',
+    med_visit_summary AS content,
+    visit_category AS category
+  FROM viewVisitsPetsVets
+  WHERE visit_category like 'medical%'
+  UNION
+  SELECT 'test' AS 'group', 
+    test_id || '_' || 'test' AS id,
+    pet_name,
+    vet_name,
+    test_date_performed AS 'start',
+    test_name AS content,
+    test_category AS category
+  FROM viewTestsPetsVets
+  WHERE test_category like 'medical%'")
+
+dbSendStatement(mydb, "CREATE VIEW viewMedHistTimeline AS
+  SELECT 'med' AS 'group', 
+    visit_id || '_' || 'med' AS id,
+    pet_name,
+    vet_name,
+    visit_date AS 'start',
+    med_visit_summary AS content,
+    visit_category AS category
+    FROM viewVisitsPetsVets
+    WHERE visit_category like 'medical%'
+    UNION
+    SELECT 'test' AS 'group', 
+    test_id || '_' || 'test' AS id,
+    pet_name,
+    vet_name,
+    test_date_performed AS 'start',
+    test_name AS content,
+    test_category AS category
+  FROM viewTestsPetsVets
+  WHERE test_category like 'medical%'")
 
 dbSendStatement(mydb, "CREATE VIEW viewVisitsVets AS
   SELECT dimVisits.visit_id,
@@ -359,40 +358,7 @@ dbSendStatement(mydb, "CREATE VIEW viewMedsPetsVets AS
   LEFT JOIN dimVets
   ON dimMeds.vet_id = dimVets.vet_id")
 
-dbSendStatement(mydb, "CREATE VIEW viewVisitsPetsVets AS
-  SELECT dimVisits.visit_id,
-     dimVisits.visit_date,
-     dimVisits.visit_weight,
-     dimVisits.visit_receipt,
-     dimVisits.visit_notes,
-     dimVisits.med_visit_summary,
-     dimVisits.routine_visit_summary,
-     dimVisits.visit_category,
-     dimVisits.visit_doctor,
-     dimVisits.visit_exam_doc,
-     dimVisits.pet_id,
-     dimVisits.vet_id,
-     dimPets.pet_name,
-     dimPets.pet_dob,
-     dimPets.pet_species,
-     dimPets.pet_breed,
-     dimPets.pet_sex,
-     dimPets.pet_color,
-     dimVets.vet_name,
-     dimVets.vet_address,
-     dimVets.vet_city,
-     dimVets.vet_state,
-     dimVets.vet_zip,
-     dimVets.vet_phone,
-     dimVets.vet_website,
-     dimVets.vet_email,
-     dimVets.vet_med_rec_site
-  FROM dimVisits
-  INNER JOIN dimPets
-  ON dimVisits.pet_id = dimPets.pet_id
-  INNER JOIN dimVets
-  ON dimVisits.vet_id = dimVets.vet_id")
-
+## Views used by other views:
 dbSendStatement(mydb, "CREATE VIEW viewTestsPetsVets AS
   SELECT dimTests.test_id,
     dimTests.test_name,
@@ -464,66 +430,41 @@ INNER JOIN dimPets
 ON dimVaccines.pet_id = dimPets.pet_id
 INNER JOIN dimVets
 ON dimVaccines.vet_id = dimVets.vet_id")
+##
 
-dbSendStatement(mydb, "DROP VIEW viewRoutineMedHistTimeline")
-dbGetQuery(mydb, "SELECT * FROM viewRoutineMedHistTimeline")
-dbSendStatement(mydb, "CREATE VIEW viewRoutineMedHistTimeline AS
-  SELECT 'routine' AS 'group', 
-    visit_id || '_' || 'routine' AS id,
-    pet_name,
-    vet_name,
-    visit_date AS 'start',
-    routine_visit_summary AS content,
-    visit_category AS category
-  FROM viewVisitsPetsVets
-  WHERE visit_category like '%routine' AND routine_visit_summary not like 'Initial visit'
-  UNION
-  SELECT 'med' AS 'group', 
-    visit_id || '_' || 'med' AS id,
-    pet_name,
-    vet_name,
-    visit_date AS 'start',
-    med_visit_summary AS content,
-    visit_category AS category
-  FROM viewVisitsPetsVets
-  WHERE visit_category like 'medical%'
-  UNION
-  SELECT 'test' AS 'group', 
-    test_id || '_' || 'test' AS id,
-    pet_name,
-    vet_name,
-    test_date_performed AS 'start',
-    test_name AS content,
-    test_category AS category
-  FROM viewTestsPetsVets
-  WHERE test_category like 'medical%'")
-
-dbSendStatement(mydb, "DROP VIEW viewMedHistTimeline")
-dbGetQuery(mydb, "SELECT * FROM viewMedHistTimeline")
-
-dbSendStatement(mydb, "CREATE VIEW viewMedHistTimeline AS
-  SELECT 'med' AS 'group', 
-    visit_id || '_' || 'med' AS id,
-    pet_name,
-    vet_name,
-    visit_date AS 'start',
-    med_visit_summary AS content,
-    visit_category AS category
-    FROM viewVisitsPetsVets
-    WHERE visit_category like 'medical%'
-    UNION
-    SELECT 'test' AS 'group', 
-    test_id || '_' || 'test' AS id,
-    pet_name,
-    vet_name,
-    test_date_performed AS 'start',
-    test_name AS content,
-    test_category AS category
-  FROM viewTestsPetsVets
-  WHERE test_category like 'medical%'")
-
-dbSendStatement(mydb, "DROP VIEW viewVaccineHistTimeline")
-dbGetQuery(mydb, "SELECT * FROM viewVaccineHistTimeline")
+dbSendStatement(mydb, "CREATE VIEW viewVisitsPetsVets AS
+  SELECT dimVisits.visit_id,
+     dimVisits.visit_date,
+     dimVisits.visit_weight,
+     dimVisits.visit_receipt,
+     dimVisits.visit_notes,
+     dimVisits.med_visit_summary,
+     dimVisits.routine_visit_summary,
+     dimVisits.visit_category,
+     dimVisits.visit_doctor,
+     dimVisits.visit_exam_doc,
+     dimVisits.pet_id,
+     dimVisits.vet_id,
+     dimPets.pet_name,
+     dimPets.pet_dob,
+     dimPets.pet_species,
+     dimPets.pet_breed,
+     dimPets.pet_sex,
+     dimPets.pet_color,
+     dimVets.vet_name,
+     dimVets.vet_address,
+     dimVets.vet_city,
+     dimVets.vet_state,
+     dimVets.vet_zip,
+     dimVets.vet_phone,
+     dimVets.vet_website,
+     dimVets.vet_email,
+     dimVets.vet_med_rec_site
+  FROM dimVisits
+  INNER JOIN dimPets
+  ON dimVisits.pet_id = dimPets.pet_id
+  INNER JOIN dimVets
+  ON dimVisits.vet_id = dimVets.vet_id")
 
 dbSendStatement(mydb, "CREATE VIEW viewVaccineHistTimeline AS
   SELECT pet_name,
@@ -553,52 +494,5 @@ dbSendStatement(mydb, "CREATE VIEW viewVaccineHistTimeline AS
   FROM viewTestsPetsVets
   WHERE test_current_flag IS NOT NULL")
 
-test <- dbGetQuery(mydb, "SELECT * FROM dimVisits")
+dbDisconnect(mydb)
 
-# "viewVisitsPets",
-# "viewRoutineMedHistTimeline",
-# "viewMedHistTimeline", 
-# "viewVisitsVets", 
-# "viewVisitsTests", 
-# "viewVisitsMeds", 
-# "viewMedsPetsVets", 
-# "viewVisitsPetsVets", 
-# "viewVaccineHistTimeline"
-# 
-# 
-# dbSendQuery(mydb, "CREATE TRIGGER create_date AFTER INSERT 
-#            ON dimPets
-#             BEGIN
-#             UPDATE dimPets SET created_date = datetime('now') WHERE pet_id = new.pet_id;
-#             END;")
-# 
-# dbSendStatement(mydb, "CREATE TRIGGER create_date_vets AFTER INSERT 
-#                 ON dimVets
-#                 BEGIN
-#                 UPDATE dimVets SET created_date = datetime('now') WHERE vet_id = new.vet_id;
-#                 END;")
-# 
-# dbSendQuery(mydb, "CREATE TRIGGER create_date_meds AFTER INSERT 
-#             ON dimMeds
-#             BEGIN
-#             UPDATE dimMeds SET created_date = datetime('now') WHERE med_id = new.med_id;
-#             END;")
-# 
-# dbSendQuery(mydb, "CREATE TRIGGER create_date_tests AFTER INSERT 
-#            ON dimTests
-#             BEGIN
-#             INSERT INTO dimTests(created_date) VALUES (datetime('now'));
-#             END;")
-# 
-# dbSendQuery(mydb, "CREATE TRIGGER create_date_vac AFTER INSERT 
-#            ON dimVaccines
-#             BEGIN
-#             INSERT INTO dimVaccines(created_date) VALUES (datetime('now'));
-#             END;")
-# 
-# dbSendQuery(mydb, "CREATE TRIGGER create_date_visits AFTER INSERT 
-#            ON dimVisits
-#             BEGIN
-#             INSERT INTO dimVisits(created_date) VALUES (datetime('now'));
-#             END;")
-# 
