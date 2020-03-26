63273281a057aec980161b1085f9fbc71c6877ac
nadia <nadi1990@gmail.com>
2020-03-25 23:49:56 -0400

    dependency check

--- run_requirements.R
+++ run_requirements.R
@@ -12,7 +12,7 @@ pkgs <- readLines("requirements.txt")
 install_missing <- function(pkg) {
     if (!requireNamespace(pkg, quietly = TRUE)) {
         cat(paste(Sys.time(), "Installing package:", pkg, "\n"))
-        install.packages(pkg, repos = "https://cloud.r-project.org/")
+        install.packages(pkg, repos = "https://cloud.r-project.org/", dependencies = TRUE)
     } else {
         cat(paste("Skipping already installed package:", pkg, "\n"))
     }
