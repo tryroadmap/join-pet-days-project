3ef3be16966e9e877682379a117c45d6ee0489c3
nadia <nadi1990@gmail.com>
2020-04-04 13:25:40 -0400

    add data dir and pdf view

--- server.R
+++ server.R
@@ -17,6 +17,7 @@ source("utils.R")
 # getting the data outside of server, so data is created once 
 # and shared across all user sessions (within the same R process)
 pet_records <- read_data("PetRecords.sqlite")
+data_dir <- file.path("assets", "data")
 
 function(input, output, session) {
   # create options for pet selection radio button ####
@@ -364,16 +365,10 @@ function(input, output, session) {
   })
   
   # show exam file if visit is selected in timeline ####
-  exam_path <- file_temp("exam_pdf", tmp_dir = "www", ext = ".pdf")
-  
   output$exam <- renderUI({
     if (show_visit_details_fun() | show_routine_fun()) {
       if (!is.na(exam())) {
-        exam() %>%
-          str_replace("https://s3.amazonaws.com", "s3:/") %>%
-          get_object() %>%
-          writeBin(exam_path)
-        tags$iframe(style = "height:1400px; width:100%", src = str_sub(exam_path, 5))
+        tags$iframe(style = "height:1400px; width:100%", src = file.path(data_dir, exam()))
       } else {
         h3("No Exam Notes Available")
         }
@@ -386,21 +381,15 @@ function(input, output, session) {
       "exam.pdf"
     },
     content = function(file) {
-      file.copy(exam_path, file)
+      file.copy(file.path(data_dir, exam()), file)
     }
   )
   
   # show test results file if test is selected in timeline ####
-  test_result_path <- file_temp("test_result_pdf", tmp_dir = "www", ext = ".pdf")
-  
   output$test_results <- renderUI({
     if (show_test_results_fun()) {
       if (!is.na(test_result())) {
-        test_result() %>%
-          str_replace("https://s3.amazonaws.com", "s3:/") %>%
-          get_object() %>%
-          writeBin(test_result_path)
-        tags$iframe(style = "height:1400px; width:100%", src = str_sub(test_result_path, 5))
+        tags$iframe(style = "height:1400px; width:100%", src = file.path(data_dir, test_result()))
       } else {
         h3("No Test Results Available")
         }
@@ -413,8 +402,9 @@ function(input, output, session) {
       "test_result.pdf"
     },
     content = function(file) {
-      file.copy(test_result_path, file)
-    }
+      file.copy(file.path(data_dir, test_result()), file)
+    },
+    contentType = NA
   )
   
   # create current meds data table ####
@@ -520,16 +510,10 @@ function(input, output, session) {
   outputOptions(output, "show_vaccine_cert", suspendWhenHidden = FALSE)
   
   # show vaccine cert if vaccine is selected in timeline ####
-  vaccine_cert_path <- file_temp("vaccine_cert_pdf", tmp_dir = "www", ext = ".pdf")
-  
   output$vaccine_cert <- renderUI({
     if (!is.null(values$vacc_tl_selected)) {
       if (!is.na(cert())) {
-        cert() %>% 
-          str_replace("https://s3.amazonaws.com", "s3:/") %>%
-          get_object() %>% 
-          writeBin(vaccine_cert_path)
-        tags$iframe(style = "height:1400px; width:100%", src = str_sub(vaccine_cert_path, 5))
+        tags$iframe(style = "height:1400px; width:100%", src = file.path(data_dir, cert()))
       } else {
         h3("No Vaccine Certificate Available")
         }
@@ -542,13 +526,13 @@ function(input, output, session) {
       "vaccine_cert.pdf"
     },
     content = function(file) {
-      file.copy(vaccine_cert_path, file)
+      file.copy(file.path(data_dir, cert()), file)
     }
   )
   
-  # delete session files when the application exits (when runApp exits) 
+  # delete session files when the application exits (when runApp exits) -- no session files
   # or after each user session ends
-  onStop(function()
-    unlink(c(test_result_path, exam_path, vaccine_cert_path)))
+  # onStop(function()
+  #   unlink(c(test_result_path, exam_path, vaccine_cert_path)))
   
 }
